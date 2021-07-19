3ae1030358a9aaaebef6a40e36b5bbf4e34ed165
GRallaX <alexey2141@gmail.com>
2021-07-19 22:29:01 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <App />
     </Provider>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
